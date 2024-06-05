51dd660a69c7688ebf04c11f68d6d73ef1f614d5
musabbir-m <musabbir313@gmail.com>
2024-06-06 02:02:19 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 const App=()=> {
 
-  return <h1 className="">Hello world</h1>;
+  return (
+    <main className="bg-black">
+     <Navbar/>
+     <h2 className="sm:text-yellow-500">Hi</h2>
+     <Hero/> 
+     <Highlights/>
+
+    </main>
+  )
 }
 
 export default App;
